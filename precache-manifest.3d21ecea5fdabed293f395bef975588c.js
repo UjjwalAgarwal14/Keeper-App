9158c59b460d0834c56b05445840f25b10c2f56a
UjjwalAgarwal14 <14ujjwal32@gmail.com>
2022-08-23 00:06:49 +0530

    Updates

---
+++ precache-manifest.3d21ecea5fdabed293f395bef975588c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4abd7e15458769699127b9eaf34b7079",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/keeper-app/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "b132a9fb5a377074592e",
+    "url": "/keeper-app/static/js/main.c1ecc65b.chunk.js"
+  },
+  {
+    "revision": "3f5fcd70531cb0f5d766",
+    "url": "/keeper-app/static/js/runtime-main.ad8fc91b.js"
+  }
+]);
